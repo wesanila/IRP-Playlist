db61f5f07434513f768d41df5dfeb134340dbb94
Sunny Vuyyuri <sunny.suncode@outlook.com>
2025-10-10 19:13:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const fadeElems = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+fadeElems.forEach(el => observer.observe(el));
